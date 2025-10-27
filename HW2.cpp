533f6640f32c2c9e2403d7effd58db468025b852
s1140353-dot <s1140353@dmhs.kh.edu.tw>
2025-10-27 16:11:53 +0800

    Add files via upload

---
+++ HW2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int age= 15;
+    double height = 161
+    std::string name = "congwai琮崴";
+    bool is_student = false;
+    std::cout << "姓名: "<< name << std::end1;
+    std::cout << "身高: "<< height << std::end1;
+    std::cout << "年齡: "<< age << std::end1;
+    std::cout << "是否為學生: "<< is_student << std::end1;
+    
+    return 0;
+}
