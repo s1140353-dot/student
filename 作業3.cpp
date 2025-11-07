cf0b85994d4612547e454ad8d142f0eeecece0dc
s1140353-dot <s1140353@dmhs.kh.edu.tw>
2025-11-07 11:48:38 +0800

    Add files via upload

---
+++ 作業3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"吃排骨飯" << std::endl;
+    std::cout<<"吃 排 骨 飯" << std::endl;
+    std::cout<<"吃\t排\t骨\t飯" << std::endl;
+    std::cout<<"吃\n排\n骨\n飯" << std::endl;
+    std::cout<<"吃排骨飯" << std::endl;
+    std::cout<<"\\吃排骨飯\\" << std::endl;
+
+    return 0;
+}
